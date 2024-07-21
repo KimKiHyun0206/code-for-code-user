7b7fc2b09de99bf020140f35e4cef4a2f5c38e7d
usr <rlarlgus0206@naver.com>
2024-07-21 19:36:27 +0900

    init commit

---
+++ src/main/java/com/user/codeforcodeuser/CodeForCodeUserApplication.java
@@ -0,0 +1,13 @@
+package com.user.codeforcodeuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeForCodeUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeForCodeUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/user/codeforcodeuser/CodeForCodeUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.codeforcodeuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeForCodeUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
