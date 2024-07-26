3116ebf4e791000a27404a229f5dc5f087f30143
usr <rlarlgus0206@naver.com>
2024-07-26 15:52:31 +0900

    update : error message

--- src/main/java/com/user/codeforcodeuser/exception/UserNotFoundException.java
+++ src/main/java/com/user/codeforcodeuser/exception/UserNotFoundException.java
@@ -4,4 +4,12 @@ public class UserNotFoundException extends BusinessException {
     public UserNotFoundException() {
         super(ErrorMessage.USER_NOT_FOUND_ERROR);
     }
+
+    public UserNotFoundException(String reason) {
+        super((ErrorMessage.USER_NOT_FOUND_ERROR), reason);
+    }
+
+    public UserNotFoundException(ErrorMessage message, String reason) {
+        super(message, reason);
+    }
 }
